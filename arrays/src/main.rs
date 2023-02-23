1cf06820c18096b446eca4588a6c21fbd291ec4f
ZackDev <ZackDev@users.noreply.github.com>
2023-02-23 12:30:29 +0100

    added vectors

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -11,6 +11,23 @@ fn main() {
     println!("mutate()");
     mutate(&mut b, 2, 'z');
     println!("{:?}", b);
+    println!("what else?");
+    println!("check vectors...");
+    let mut v: Vec<u8> = Vec::new();
+    println!("initial vector:");
+    println!("{:?}", v);
+    v.push(16u8);
+    println!("added 16:");
+    println!("{:?}", v);
+    let i = v.pop();        // returns Option type, could return None too.
+    let j = v.pop();
+    print_some_or_none(i);
+    print_some_or_none(j);
+    let result_ok = Ok::<u8, i8>(8u8);
+    println!("{:?}", result_ok);
+    let result_err = Err::<u8, i8>(8i8);
+    println!("{:?}", result_err);
+
 }
 
 // arrays need a constant length known at compile time, therefore type needs to be known too
@@ -27,5 +44,17 @@ fn mutate(array: &mut [char; 3], pos: usize, v: char) {
 
 fn mutate_locally(mut array: [char; 3], pos: usize, v: char) {
     array[pos] = v;
-    println!("{:?}", array)
+    println!("{:?}", array);
+}
+
+fn print_some_or_none(opt: Option<u8>) {
+    // cool, match <T> { type } is possible :>
+    match opt {
+        Some(i) => println!("{}", i),
+        None => println!("None"),
+    }
+}
+
+fn arrow() -> u8 {
+    return 8u8;
 }
