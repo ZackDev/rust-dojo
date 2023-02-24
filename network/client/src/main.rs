aef0b0fd4bcaf7248170ae33b3967a7b6ad9ab7b
ZackDev <ZackDev@users.noreply.github.com>
2023-02-24 18:18:34 +0100

    added Arc

--- network/client/src/main.rs
+++ network/client/src/main.rs
@@ -1,18 +1,22 @@
 use std::io::Write;
 use std::net::{TcpStream};
+use std::sync::Arc;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let h1 = thread::spawn(|| {
+    let msg = String::from("Hello, World!");
+    let msg_arc: Arc<String> = Arc::new(msg);
+    let msg_arc2 = msg_arc.clone();
+    let h1 = thread::spawn(move || {
         let mut tcp_stream = TcpStream::connect("127.0.0.1:9876").unwrap();
-        let msg = String::from("Hello, World!");
-        tcp_stream.write(msg.as_bytes()).unwrap();
+        tcp_stream.write(msg_arc.as_bytes()).unwrap();
     });
 
-    let h2 = thread::spawn(|| {
+    let h2 = thread::spawn(move || {
+        thread::sleep(Duration::from_secs(5));
         let mut tcp_stream = TcpStream::connect("127.0.0.1:9876").unwrap();
-        let msg = String::from("Hello, World!");
-        tcp_stream.write(msg.as_bytes()).unwrap();
+        tcp_stream.write(msg_arc2.as_bytes()).unwrap();
     });
 
     h1.join().unwrap();
