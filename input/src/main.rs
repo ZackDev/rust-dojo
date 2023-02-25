0e1fa74379f9191d44f27c8b7bf5f3cb405916ab
ZackDev <ZackDev@users.noreply.github.com>
2023-02-25 20:24:45 +0100

    improved program flow

---
+++ input/src/main.rs
@@ -0,0 +1,50 @@
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    // input as parameters
+    let args: Vec<String> = std::env::args().collect();
+    dbg!(args);
+
+    // same, but doesn't panick on non-unicode parameters
+    let args_os: Vec<OsString> = std::env::args_os().collect();
+    dbg!(args_os);
+
+    loop {
+        let path = read_stdin();
+        match path {
+            Ok(path) => {
+                let file_read = read_and_print(path);
+                match file_read {
+                    Ok(_) => {
+                        println!("file successfully read and printed.");
+                        break;
+                    },
+                    Err(_) => {
+                        println!("file not found.");
+                    },
+                };
+            },
+            Err(e) => {
+            },
+        }
+    }
+
+}
+
+fn read_and_print(file_path: String) -> Result<(), std::io::Error> {
+    let mut file = File::open(file_path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+    println!("{}", content);
+    Ok(())
+}
+
+fn read_stdin() -> Result<String, std::io::Error>  {
+    println!("enter path to file:");
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut buffer)?;
+    Ok(buffer.trim().to_string())
+}
