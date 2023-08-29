c5462ad9ddf7b38396dcc6b664c52dfd0acb14ca
ZackDev <ZackDev@users.noreply.github.com>
2023-08-29 19:15:36 +0200

    update

--- bikestat/src/main.rs
+++ bikestat/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::read_to_string;
+use std::cmp::max;
+use std::cmp::min;
 
 const DFILE: &str = "/home/zack/biketime.csv";
 
@@ -6,9 +8,8 @@ const DFILE: &str = "/home/zack/biketime.csv";
 fn main() {
     let mut times: Vec<u32> = Vec::new();
     let mut total: u32 = u32::MIN;
-    let mut min: u32 = u32::MAX;
-    let mut max: u32 = u32::MIN;
-    let mut num_rides: u32 = u32::MIN;
+    let mut min_dist: u32 = u32::MAX;
+    let mut max_dist: u32 = u32::MIN;
 
     for line in read_to_string(DFILE).unwrap().lines() {
         let data: Vec<&str> = line.split(",").collect();
@@ -16,23 +17,20 @@ fn main() {
         times.push(time);
     }
     
-    for time in times {
-        num_rides += 1;
-        total += time;
-        if time > max {
-            max = time;
-        }
-        if time < min {
-            min = time;
-        }
+    for i in 0..times.len() {
+        total += times[i];
+        max_dist = max(max_dist, times[i]);
+        min_dist = min(min_dist, times[i]);
     }
 
-    let average = total / num_rides;
+    let num_rides: u32 = times.len() as u32;
+
+    let average: u32 = total / num_rides;
 
     println!("total:{total}");
     println!("average:{average}");
-    println!("min:{min}");
-    println!("max:{max}");
+    println!("min:{min_dist}");
+    println!("max:{max_dist}");
     println!("rides:{num_rides}");
 
 }
