afd541ce9ef8199b57853bd9cf2c51ce51ffd02e
ZackDev <ZackDev@users.noreply.github.com>
2023-08-29 10:40:27 +0200

    update

--- biketime/src/main.rs
+++ biketime/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::{fs::{File, OpenOptions}, io::Write};
+use std::fs::OpenOptions;
+use std::io::Write;
+use chrono::Datelike;
+use chrono::DateTime;
+use chrono::Utc;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -11,12 +15,19 @@ const DFILE: &str = "/home/zack/biketime.csv";
 
 fn main() {
     let args = Args::parse();
-    let mut time = String::new();
-    time.push_str(args.time.to_string().as_ref());
-    time.push_str("\n");
+    let current_time: DateTime<Utc> = Utc::now();
+    let mut line = String::new();
+    line.push_str(&current_time.year().to_string());
+    line.push_str("-");
+    line.push_str(&current_time.month().to_string());
+    line.push_str("-");
+    line.push_str(&current_time.day().to_string());
+    line.push_str(", ");
+    line.push_str(args.time.to_string().as_ref());
+    line.push_str("m\n");
     match OpenOptions::new().write(true).create(true).append(true).open(DFILE) {
         Ok(mut file) => {
-            file.write_all(time.as_ref()).unwrap();
+            file.write_all(line.as_ref()).unwrap();
         }
         Err(e) => {
 
