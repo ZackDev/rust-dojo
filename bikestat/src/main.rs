d2913d4671209e22731a83c0c82d694198ecc57c
ZackDev <ZackDev@users.noreply.github.com>
2023-08-29 19:34:52 +0200

    update

--- bikestat/src/main.rs
+++ bikestat/src/main.rs
@@ -7,7 +7,6 @@ const DFILE: &str = "/home/zack/biketime.csv";
 
 fn main() {
     let mut times: Vec<u32> = Vec::new();
-    let mut total: u32 = u32::MIN;
     let mut min_dist: u32 = u32::MAX;
     let mut max_dist: u32 = u32::MIN;
 
@@ -18,20 +17,20 @@ fn main() {
     }
     
     for i in 0..times.len() {
-        total += times[i];
         max_dist = max(max_dist, times[i]);
         min_dist = min(min_dist, times[i]);
     }
 
+    let sum_dist: u32 = times.iter().sum();
     let num_rides: u32 = times.len() as u32;
 
-    let average: u32 = total / num_rides;
+    let average: u32 = sum_dist / num_rides;
 
-    println!("total:{total}");
-    println!("average:{average}");
-    println!("min:{min_dist}");
-    println!("max:{max_dist}");
-    println!("rides:{num_rides}");
+    println!("total dist:{sum_dist}");
+    println!("average dist:{average}");
+    println!("min dist:{min_dist}");
+    println!("max dist:{max_dist}");
+    println!("num rides:{num_rides}");
 
 }
 
