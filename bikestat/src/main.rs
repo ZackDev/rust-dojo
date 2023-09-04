bfb329099a91fc1be1b7e4b50bcd0183a092ffb7
ZackDev <ZackDev@users.noreply.github.com>
2023-09-04 15:04:14 +0200

    update

--- bikestat/src/main.rs
+++ bikestat/src/main.rs
@@ -1,19 +1,30 @@
 use std::fs::read_to_string;
 use std::cmp::max;
 use std::cmp::min;
+use std::ops::Add;
 use std::path::Path;
+use std::str::FromStr;
+use chrono::DateTime;
+use chrono::Duration;
+use chrono::Utc;
+use chrono::prelude::*;
 
 fn main() {
     let dfile: &Path = Path::new("/home/zack/biketime.csv");
-    let mut dates: Vec<String> = Vec::new();
+    let mut dates: Vec<DateTime<Utc>> = Vec::new();
     let mut times: Vec<u32> = Vec::new();
     let mut min_time: u32 = u32::MAX;
     let mut max_time: u32 = u32::MIN;
 
     for line in read_to_string(dfile).unwrap().lines() {
         let data: Vec<&str> = line.split(",").collect();
-        let date: String = data[0].trim().parse().unwrap();
         let time: u32 = data[1].trim().parse().unwrap();
+        let date_str: String = data[0].trim().parse().unwrap();
+        let date_split: Vec<&str> = date_str.split("-").collect();
+        let year: i32 = FromStr::from_str(date_split[0]).unwrap();
+        let month: u32 = FromStr::from_str(date_split[1]).unwrap();
+        let day: u32 = FromStr::from_str(date_split[2]).unwrap();
+        let date = Utc.with_ymd_and_hms(year, month, day, 0, 0, 0).unwrap();
         dates.push(date);
         times.push(time);
     }
@@ -27,6 +38,19 @@ fn main() {
     let num_rides: u32 = times.len() as u32;
 
     let average: f64 = sum_time as f64 / num_rides as f64;
+
+    let mut frequency: Vec<usize> = Vec::new();
+
+    let mut date_iter = dates[0];
+
+    while date_iter <= dates[dates.len()-1] {
+        frequency.push(dates.iter().filter(|&x| *x == date_iter).count());
+        date_iter = date_iter + Duration::days(1);
+    }
+    
+    
+
+    println!("{:#?}", frequency);
 
     println!("first run:\t{}", dates[0]);
     println!("last run:\t{}", dates[dates.len()-1]);
