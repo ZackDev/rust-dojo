c66ec08e54442f66fd0e57fdb537aee27e16b2d3
ZackDev <ZackDev@users.noreply.github.com>
2023-09-06 18:07:54 +0200

    update

--- resultmatching/src/main.rs
+++ resultmatching/src/main.rs
@@ -2,8 +2,10 @@ use std::{str::FromStr, process::exit};
 
 fn main() {
     let s: String = "12".to_string();
-
+    /* define i uninitialized */
     let i: i32;
+
+    /* assign a value to i inside the Ok arm */
     match FromStr::from_str(&s) {
         Ok(v) => {
             i = v;
