619086fee9b5d88d97e4ea111e81046029a5aade
ZackDev <ZackDev@users.noreply.github.com>
2023-09-30 18:53:27 +0200

    update

--- referencing_3/src/main.rs
+++ referencing_3/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     // trying to pass the reference of a value to a function
-    let mut i: u32 = 4;
+    let mut i: u32 = 4;     // a mutable u32 variable
     println!("{i}");
 
-    add(&mut i, 1);
+    add(&mut i, 1); // a mutable 'borrow' of i
     println!("{i}");
 
-    add(&mut i, 1);
+    add(&mut i, 1); // another mutalble 'borrow'
     println!("{i}");
 }
 
-fn add(r: &mut u32, to_add: u32) {
+fn add(r: &mut u32, to_add: u32) {  // r = mutable u32 'borrow', to_add = variable u32
     *r += to_add;
 }
