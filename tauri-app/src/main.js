910a003830b94c11000518c1898749e275a646cf
ZackDev <ZackDev@users.noreply.github.com>
2024-04-24 20:47:15 +0200

    update

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -3,6 +3,19 @@ const { invoke } = window.__TAURI__.tauri;
 let timesInputEl;
 let timesMsgEl;
 
+/*
+stats checkboxes
+*/
+let currentDateCb;
+let firstRunCb;
+let lastRunCb;
+let totalTimeCb;
+let averageTimeCb;
+let minAndMaxTimeCb;
+let numRidesCb;
+let frequencyCb;
+let durationCb;
+
 async function addtime() {
   timesMsgEl.textContent = await invoke("addtime", { time: timesInputEl.value });
 }
@@ -11,6 +24,10 @@ window.addEventListener("DOMContentLoaded", () => {
   timesInputEl = document.querySelector("#times-input");
   timesMsgEl = document.querySelector("#times-container");
   
+  /* TODO add checkbox references */
+
+  /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
+
   document.querySelector("#times-form").addEventListener("submit", (e) => {
     e.preventDefault();
     addtime();
