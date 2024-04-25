52017694f7fd80af8706ffd74661b3920cc90f6a
ZackDev <ZackDev@users.noreply.github.com>
2024-04-25 18:24:19 +0200

    update

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,49 +1,84 @@
 const { invoke } = window.__TAURI__.tauri;
 
+let cbs;
+
 let timesInputEl;
 let timesMsgEl;
 
 /*
 stats checkboxes
 */
-let cCb;
-let OneCb;
-let lCb;
-let tCb;
-let aCb;
-let xCb;
-let nCb;
-let fCb;
-let dCb;
-
-let cbs = [cCb, OneCb, lCb, tCb, aCb, xCb, nCb, fCb, dCb];
+let c;
+let o;
+let l;
+let t;
+let a;
+let x;
+let n;
+let f;
+let d;
 
 async function addtime() {
   timesMsgEl.textContent = await invoke("addtime", { time: timesInputEl.value });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  /* TODO add checkbox references */
-  cCb = document.querySelector("current-date-cb");
-  OneCb = document.querySelector("first-run-cb");
-  lCb = document.querySelector("last-run-cb");
-  tCb = document.querySelector("total-time-cb");
-  aCb = document.querySelector("average-time-cb");
-  xCb = document.querySelector("min-max-time-cb");
-  nC = document.querySelector("num-rides-cb");
-  fCb = document.querySelector("frequency-cb");
-  dCb = document.querySelector("duration-cb");
-  
-  /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
-  cbs.forEach(cb => {
-    cb.addEventListener("onchange", (e) => {
-      e.preventDefault();
-      // TODO build the stats string and pass it to the backend
-    })
-  })
+function getCheckedBoxes() {
+  let statStr = "";
+  if (c.checked) {
+    statStr += "c";
+  }
+  if (o.checked) {
+    statStr += "1";
+  }
+  if (l.checked) {
+    statStr += "l";
+  }
+  if (t.checked) {
+    statStr += "t";
+  }
+  if (a.checked) {
+    statStr += "a";
+  }
+  if (x.checked) {
+    statStr += "x";
+  }
+  if (n.checked) {
+    statStr += "n";
+  }
+  if (f.checked) {
+    statStr += "f";
+  }
+  if (d.checked) {
+    statStr += "d";
+  }
+  return statStr;
+}
 
+window.addEventListener("DOMContentLoaded", () => {
   timesInputEl = document.querySelector("#times-input");
   timesMsgEl = document.querySelector("#times-container");
+
+  /* TODO add checkbox references */
+  
+  c = document.querySelector("#current-date-cb");
+  o = document.querySelector("#first-run-cb");
+  l = document.querySelector("#last-run-cb");
+  t = document.querySelector("#total-time-cb");
+  a = document.querySelector("#average-time-cb");
+  x = document.querySelector("#min-max-time-cb");
+  n = document.querySelector("#num-rides-cb");
+  f = document.querySelector("#frequency-cb");
+  d = document.querySelector("#duration-cb");
+  
+  cbs = [c, o, l, t, a, x, n, f, d];
+
+  /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
+  cbs.forEach((cb) => {
+    cb.addEventListener("click", (e) => {
+      // timesMsgEl.textContent = getCheckedBoxes();
+      let statStr = getCheckedBoxes();
+    });
+  });
 
   document.querySelector("#times-form").addEventListener("submit", (e) => {
     e.preventDefault();
