b6e25debbe1558ed5c74bc5b164fdbbf654cf659
ZackDev <ZackDev@users.noreply.github.com>
2024-04-25 07:09:43 +0200

    update

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -6,27 +6,38 @@ let timesMsgEl;
 /*
 stats checkboxes
 */
-let currentDateCb;
-let firstRunCb;
-let lastRunCb;
-let totalTimeCb;
-let averageTimeCb;
-let minAndMaxTimeCb;
-let numRidesCb;
-let frequencyCb;
-let durationCb;
+let cCb;
+let OneCb;
+let lCb;
+let tCb;
+let aCb;
+let xCb;
+let nCb;
+let fCb;
+let dCb;
 
 async function addtime() {
   timesMsgEl.textContent = await invoke("addtime", { time: timesInputEl.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  /* TODO add checkbox references */
+  cCb = document.querySelector();
+  OneCb = document.querySelector();
+  lCb = document.querySelector();
+  tCb = document.querySelector();
+  aCb = document.querySelector();
+  xCb = document.querySelector();
+  nC = document.querySelector();
+  fCb = document.querySelector();
+  dCb = document.querySelector();
+  
+  /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
+  
   timesInputEl = document.querySelector("#times-input");
   timesMsgEl = document.querySelector("#times-container");
   
-  /* TODO add checkbox references */
 
-  /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
 
   document.querySelector("#times-form").addEventListener("submit", (e) => {
     e.preventDefault();
