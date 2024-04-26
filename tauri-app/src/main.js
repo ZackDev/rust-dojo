361db77c849d75df0da5d59959de8f7c8962e915
ZackDev <ZackDev@users.noreply.github.com>
2024-04-26 18:43:25 +0200

    update

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -1,57 +1,22 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let cbs;
+let statsBoxes;
 
 let timesInputEl;
 let timesMsgEl;
-
-/*
-stats checkboxes
-*/
-let c;
-let o;
-let l;
-let t;
-let a;
-let x;
-let n;
-let f;
-let d;
 
 async function addtime() {
   timesMsgEl.textContent = await invoke("addtime", { time: timesInputEl.value });
 }
 
-function getCheckedBoxes() {
-  let statStr = "";
-  if (c.checked) {
-    statStr += "c";
-  }
-  if (o.checked) {
-    statStr += "1";
-  }
-  if (l.checked) {
-    statStr += "l";
-  }
-  if (t.checked) {
-    statStr += "t";
-  }
-  if (a.checked) {
-    statStr += "a";
-  }
-  if (x.checked) {
-    statStr += "x";
-  }
-  if (n.checked) {
-    statStr += "n";
-  }
-  if (f.checked) {
-    statStr += "f";
-  }
-  if (d.checked) {
-    statStr += "d";
-  }
-  return statStr;
+function getStatsBoxes() {
+  let boxes = document.querySelectorAll("input[stats]");
+  return boxes;
+}
+
+function getCheckedStatsBoxes() {
+  let boxes = document.querySelectorAll("input[stats]:checked");
+  return boxes;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -60,23 +25,17 @@ window.addEventListener("DOMContentLoaded", () => {
 
   /* TODO add checkbox references */
   
-  c = document.querySelector("#current-date-cb");
-  o = document.querySelector("#first-run-cb");
-  l = document.querySelector("#last-run-cb");
-  t = document.querySelector("#total-time-cb");
-  a = document.querySelector("#average-time-cb");
-  x = document.querySelector("#min-max-time-cb");
-  n = document.querySelector("#num-rides-cb");
-  f = document.querySelector("#frequency-cb");
-  d = document.querySelector("#duration-cb");
-  
-  cbs = [c, o, l, t, a, x, n, f, d];
+  statsBoxes = getStatsBoxes();
 
   /* TODO call 'stats' backend when ANY of the checkboxes changes and pass every checked stats */
-  cbs.forEach((cb) => {
-    cb.addEventListener("click", (e) => {
-      // timesMsgEl.textContent = getCheckedBoxes();
-      let statStr = getCheckedBoxes();
+  statsBoxes.forEach((sb) => {
+    sb.addEventListener("click", (e) => {
+      let statsStr = "";
+      let checkedStatsBoxes = getCheckedStatsBoxes();
+      checkedStatsBoxes.forEach((b) => {
+        statsStr += b.value;
+      });
+      timesMsgEl.textContent = statsStr;
     });
   });
 
