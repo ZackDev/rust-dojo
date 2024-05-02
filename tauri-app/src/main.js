1be5c616db01086960dc47e3b1204cee6777e551
ZackDev <ZackDev@users.noreply.github.com>
2024-05-02 19:06:29 +0200

    update

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -69,60 +69,50 @@ function getCheckedStatsBoxes() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  statsControls = document.querySelector("#stats-controls");
+  statsControls = document.querySelector("#stats-container");
   timesInputEl = document.querySelector("#times-input");
   msgEl = document.querySelector("#message-container");
   
   stats.forEach((s) => {
     let st;
     if (s[2] == "f" || s[2] == "d") {
-      st = document.createElement("canvas");
-      st.id = s[1] + "-stats";
+      getstats(s[2]).then(
+        (v) => {
+          setstat(s[1], v, s[2]);
+        },
+        (f) => {
+  
+        }
+      );
+
     } else {
       st = document.createElement("div");
       st.id = s[1] + "-stats";
       st.classList.add("stats-display");
       st.style.width = "auto";
       st.style.fontSize = "20px";
+      
+      let co = document.createElement("div");
+      co.classList.add("row", "stats-row");
+  
+      let la = document.createElement("label");
+      la.textContent = s[0];
+      la.setAttribute("for", s[1] + "-cb");
+      la.style.minWidth = "250px";
+      la.style.textAlign = "end";
+      
+      getstats(s[2]).then(
+        (v) => {
+          setstat(s[1], v, s[2]);
+        },
+        (f) => {
+  
+        }
+      );
+  
+      co.append(la, st);
+      statsControls.append(co);
     }
-    let co = document.createElement("div");
-    co.classList.add("row", "stats-row");
-
-
-
-    let la = document.createElement("label");
-    la.textContent = s[0];
-    la.setAttribute("for", s[1] + "-cb");
-    la.style.minWidth = "250px";
-    la.style.textAlign = "end";
-    
-    let cb = document.createElement("input");
-    cb.id = s[1] + "-cb";
-    cb.type = "checkbox";
-    cb.setAttribute("stats", "stats");
-    cb.value = s[2];
-    cb.style.width = "20px";
-    cb.style.height = "20px";
-
-    cb.addEventListener("click", (e) => {
-      if (cb.checked) {
-        getstats(cb.value).then(
-          (v) => {
-            setstat(s[1], v, cb.value);
-          },
-          (f) => {
-
-          }
-        );
-      }
-      else {
-        st.textContent = "";
-      }
-    });
-
-    co.append(st, la, cb);
-    statsControls.append(co);
-
   });
 
   document.querySelector("#times-form").addEventListener("submit", (e) => {
